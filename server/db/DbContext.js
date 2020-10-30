c9ee1a2f2821bdd423318eeda1653f9e14a07458
Cunningdan <cunningdan@hotmail.com>
2020-10-30 11:30:53 -0600

    commit

--- server/db/DbContext.js
+++ server/db/DbContext.js
@@ -2,13 +2,11 @@ import mongoose from "mongoose";
 import CharacterSchema from "../models/Character";
 import ProfileSchema from "../models/Profile";
 import CommentSchema from "../models/Comment";
-import UserSchema from "../models/User";
 
 class DbContext {
   Characters = mongoose.model("Character", CharacterSchema);
   Profile = mongoose.model("Profile", ProfileSchema);
   Comments = mongoose.model("Comment", CommentSchema);
-  Users = mongoose.model("User", UserSchema)
 }
 
 export const dbContext = new DbContext();
